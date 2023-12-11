194243d895a83272f53f97d3f56ec432dbeb31fc
Denzel Pressey <pressey90@gmail.com>
2023-12-11 16:00:13 -0500

    added the main script file and set permissions

---
+++ content.js
@@ -0,0 +1,5 @@
+(function (){
+    // TODO: add page functions 
+    getVideosFromPlaylist(); // gets the thumbnail, title, and channel name of the video
+    displayVideosInExtensionModal(); 
+})();
