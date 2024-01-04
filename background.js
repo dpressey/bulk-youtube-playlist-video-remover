5185337d57604de582f778272dae9c5eccdd50eb
Denzel Pressey <pressey90@gmail.com>
2024-01-04 08:45:30 -0500

    updated methods and removed code blot

--- background.js
+++ background.js
@@ -2,14 +2,16 @@
     Core Functions
 */
 
-chrome.action.onClicked.addListener(openDemoTab);
+// TODO: implement this action to open up the modal in a new tab
+// chrome.action.onClicked(function(
+//   chrome.tabs.create({ url: 'index.html' });
+// ));
 
 chrome.webNavigation.onDOMContentLoaded.addListener(async ({ tabId, url }) => {
   if (url !== 'https://*.youtube.com/*') return;
-  console.log('chrome event listener did NOT execute')
+  
   const { options } = await chrome.storage.local.get('options');
   chrome.scripting.executeScript({
-    console.log('chrome event listener IS executing')
     target: { tabId },
     files: ['content-script.js'],
     ...options
@@ -19,16 +21,3 @@ chrome.webNavigation.onDOMContentLoaded.addListener(async ({ tabId, url }) => {
 /* 
     Helper Functions
 */
-
-function openDemoTab() {
-    chrome.tabs.create({ url: 'index.html' });
-  }
-
-// chrome.webNavigation.onCompleted.addListener((details) => {
-//     if (details.url) {
-//         chrome.scripting.executeScript({
-//             target: { tabId: details.tabId },
-//             files: ['content.js']
-//         });
-//     }
-// }, { url: [{ urlMatches: 'https://*.youtube.com/*' }, { urlMatches: 'https://www.hulu.com/*' }] });
